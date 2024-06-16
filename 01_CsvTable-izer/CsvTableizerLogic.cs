f74b085bb6cec9e6f0875e742e6bc007e0152c4f
Falko Gebel <falko-gebel@live.de>
2024-06-16 23:50:32 +0200

    - worked on CSV Table-izer

---
+++ 01_CsvTable-izer/CsvTableizerLogic.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_CsvTable_izer
+{
+    public class CsvTableizerLogic
+    {
+        public static List<string> ToTable(List<string> CsvLines)
+        {
+            return CsvLines;
+        }
+    }
+}
