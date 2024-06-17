69069c32ea9cf8e3c69ef897b5fda615778f5d0d
Falko Gebel <falko-gebel@live.de>
2024-06-17 20:09:24 +0200

    - worked on and finished FizzBuzz

---
+++ 02_FizzBuzz/FizzBuzzLogic.cs
@@ -0,0 +1,34 @@
+﻿
+namespace _02_FizzBuzz
+{
+    public class FizzBuzzLogic
+    {
+        /*
+        public static string FizzBuzz(int v)
+        {
+            string output = string.Empty;
+
+            if (v % 3 == 0)
+                output += "Fizz";
+
+            if (v % 5 == 0)
+                output += "Buzz";
+
+            return output != string.Empty ? output : v.ToString();
+        }
+        */
+
+        public static string FizzBuzz(int v, bool useVariant = false)
+        {
+            string output = string.Empty;
+
+            if (v % 3 == 0 || (useVariant && v.ToString().Contains('3')))
+                output += "Fizz";
+
+            if (v % 5 == 0 || (useVariant && v.ToString().Contains('5')))
+                output += "Buzz";
+
+            return output != string.Empty ? output : v.ToString();
+        }
+    }
+}
