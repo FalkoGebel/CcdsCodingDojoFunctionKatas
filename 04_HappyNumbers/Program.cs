2a32dfc72538dba4bc50567a62ddb1edba8294c7
Falko Gebel <falko-gebel@live.de>
2024-06-25 18:42:54 +0200

    - worked on an finished Happy Numbers

---
+++ 04_HappyNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using _04_HappyNumbers;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Happy Numbers");
+
+        while (true)
+        {
+            Console.Write("Enter a number to check for its happiness - 'q' to quit: ");
+            string? input = Console.ReadLine();
+
+            if (input == null)
+                continue;
+
+            if (input == "q")
+                break;
+
+            try
+            {
+                int number = int.Parse(input);
+                Console.WriteLine($"{number} is a {(number > 0 && HappyNumbersLogic.IsHappy(number) ? "" : "un")}happy number.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Your input was not a valid number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Your number was way to small or way to large for this little program");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ In this repository I am presenting my solutions for the function katas of the [C
 + 01: [CSV Table-izer](https://ccd-school.de/en/coding-dojo/function-katas/csv-table-izer/ "CSV Table-izer") -> [Done](/01_CsvTable-izer/ "Project Folder")
 + 02: [FizzBuzz](https://ccd-school.de/en/coding-dojo/function-katas/fizzbuzz/ "FizzBuzz") -> [Done](/02_FizzBuzz/ "Project Folder")
 + 03: [From Roman Numerals](https://ccd-school.de/en/coding-dojo/function-katas/from-roman-numerals/ "From Roman Numerals") -> [Done](/03_FromRomanNumerals/ "Project Folder")
++ 04: [Happy Numbers](https://ccd-school.de/en/coding-dojo/function-katas/happy-numbers/ "Happy Numbers") -> [Done](/04_HappyNumbers/ "Project Folder")
