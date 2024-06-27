dc2dcf4f6b64180b812904e16959fa7724dae325
Falko Gebel <falko-gebel@live.de>
2024-06-27 23:17:08 +0200

    - worked on File Path Helper

---
+++ 06_FilePathHelper/FilePathHelperLogic.cs
@@ -0,0 +1,28 @@
+﻿namespace _06_FilePathHelper
+{
+    public static class FilePathHelperLogic
+    {
+        public static object GetAbsolutePath(string input)
+        {
+            string basePath = string.Empty;
+
+            if (input.StartsWith('~'))
+                basePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            else if (input.StartsWith('.'))
+                basePath = Directory.GetCurrentDirectory();
+
+            List<string> outputParts = [.. basePath.Split(Path.DirectorySeparatorChar)];
+            List<string> inputParts = [.. input.Split(Path.DirectorySeparatorChar)];
+
+            for (int i = 1; i < inputParts.Count; i++)
+                outputParts.Add(inputParts[i]);
+
+            if (outputParts.Any(p => p.All(c => c == '.')))
+                throw new ArgumentException("Invalid relative path");
+
+            string output = Path.Join([.. outputParts]);
+
+            return output;
+        }
+    }
+}
---
+++ Tests/FilePathHelperTests.cs
@@ -0,0 +1,49 @@
+﻿using _06_FilePathHelper;
+using FluentAssertions;
+
+namespace Tests
+{
+    [TestClass]
+    public class FilePathHelperTests
+    {
+        [DataTestMethod]
+        [DataRow($"~\\Downloads\\mountains.jpg", "\\Downloads\\mountains.jpg")]
+        public void Input_valid_relative_path_and_get_absolute_home_path(string input, string expected)
+        {
+            List<string> parts = [.. Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).Split(Path.DirectorySeparatorChar)];
+
+            foreach (string p in expected.Split(Path.DirectorySeparatorChar))
+                parts.Add(p);
+
+            expected = Path.Join([.. parts]);
+
+            FilePathHelperLogic.GetAbsolutePath(input).Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow(".\\Downloads\\mountains.jpg", "\\Downloads\\mountains.jpg")]
+        public void Input_valid_relative_path_and_get_absolute_working_path(string input, string expected)
+        {
+            List<string> parts = [.. Directory.GetCurrentDirectory().Split(Path.DirectorySeparatorChar)];
+
+            foreach (string p in expected.Split(Path.DirectorySeparatorChar))
+                parts.Add(p);
+
+            expected = Path.Join([.. parts]);
+
+            FilePathHelperLogic.GetAbsolutePath(input).Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow("~\\.\\mountains.jpg")]
+        [DataRow(".\\.\\mountains.jpg")]
+        [DataRow("~abc\\path\\mountains.jpg")]
+        [DataRow(".abc\\path\\mountains.jpg")]
+        [DataRow("~~\\path\\mountains.jpg")]
+        public void Input_invalid_relative_path_and_get_argument_exception(string input)
+        {
+            Action act = () => FilePathHelperLogic.GetAbsolutePath(input);
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
