836a82525395f49a7babd9998f3a9dec1fa2d32e
Falko Gebel <falko-gebel@live.de>
2024-07-15 18:39:27 +0200

    - workd on and finished Russian Peasant Multiplication

---
+++ 09_RussianPeasantMultiplication/RussianPeasantMultiplicationLogic.cs
@@ -0,0 +1,28 @@
+﻿namespace _09_RussianPeasantMultiplication
+{
+    public static class RussianPeasantMultiplicationLogic
+    {
+        public static int Mul(int first, int second)
+        {
+            if (first <= 0 || second <= 0)
+                throw new ArgumentException("Both factors has to be positive");
+
+            if (first == 1)
+                return second;
+
+            int output = 0;
+
+            do
+            {
+                if (first % 2 == 1)
+                    output += second;
+
+                first /= 2;
+                second *= 2;
+
+            } while (first > 1);
+
+            return output + second;
+        }
+    }
+}
