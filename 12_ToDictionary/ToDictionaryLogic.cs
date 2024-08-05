16498087596a7181d8614d5524b5d492da31dcb4
Falko Gebel <falko-gebel@live.de>
2024-08-05 21:59:19 +0200

    - worked on and finished ToDictionary

---
+++ 12_ToDictionary/ToDictionaryLogic.cs
@@ -0,0 +1,27 @@
+﻿namespace _12_ToDictionary
+{
+    public static class ToDictionaryLogic
+    {
+        public static IDictionary<string, string> ToDictionary(string input)
+        {
+            Dictionary<string, string> output = [];
+
+            foreach (string element in input.Split(';'))
+            {
+                string current = element;
+
+                if (current == string.Empty)
+                    continue;
+
+                int idx = current.IndexOf('=');
+
+                if (idx < 1)
+                    throw new ArgumentException("Invalid input string");
+                else
+                    output[current[..idx]] = current[(idx + 1)..];
+            }
+
+            return output;
+        }
+    }
+}
