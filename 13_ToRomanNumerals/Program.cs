ebbf26af3722bf5e86cf8e8a24c8d6837ecd96fe
Falko Gebel <falko-gebel@live.de>
2024-08-07 18:41:03 +0200

    - worked on an finished To Roman Numerals

---
+++ 13_ToRomanNumerals/Program.cs
@@ -0,0 +1,30 @@
+﻿using _13_ToRomanNumerals;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("To Roman Numerals");
+
+        while (true)
+        {
+            Console.WriteLine("Enter a positive integer between 100 and 3000 - 'q' to quit:");
+            string? input = Console.ReadLine();
+
+            if (input == null)
+                continue;
+
+            if (input == "q")
+                break;
+
+            try
+            {
+                Console.WriteLine($"Result: {ToRomanNumeralsLogic.GetRomanNumeralFromDecimal(input)}");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"Error: {ae.Message}");
+            }
+        }
+    }
+}
---
+++ Tests/ToRomanNumeralsTests.cs
@@ -0,0 +1,50 @@
+﻿using _13_ToRomanNumerals;
+using FluentAssertions;
+
+namespace Tests
+{
+    [TestClass]
+    public class ToRomanNumeralsTests
+    {
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("text")]
+        [DataRow("1text")]
+        [DataRow("-100")]
+        [DataRow("-1")]
+        [DataRow("3001")]
+        [DataRow("10000")]
+        public void Invalid_input_and_arguement_exception(string decimalNumber)
+        {
+            Action act = () => ToRomanNumeralsLogic.GetRomanNumeralFromDecimal(decimalNumber);
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [DataTestMethod]
+        [DataRow("I", "1")]
+        [DataRow("II", "2")]
+        [DataRow("III", "3")]
+        [DataRow("IV", "4")]
+        [DataRow("V", "5")]
+        [DataRow("VI", "6")]
+        [DataRow("VII", "7")]
+        [DataRow("VIII", "8")]
+        [DataRow("IX", "9")]
+        [DataRow("X", "10")]
+        [DataRow("XI", "11")]
+        [DataRow("XV", "15")]
+        [DataRow("XIX", "19")]
+        [DataRow("XLII", "42")]
+        [DataRow("L", "50")]
+        [DataRow("XCIX", "99")]
+        [DataRow("C", "100")]
+        [DataRow("M", "1000")]
+        [DataRow("MM", "2000")]
+        [DataRow("MMXIII", "2013")]
+        [DataRow("MMM", "3000")]
+        public void Valid_input_and_correct_result(string expected, string decimalNumber)
+        {
+            ToRomanNumeralsLogic.GetRomanNumeralFromDecimal(decimalNumber).Should().Be(expected);
+        }
+    }
+}
